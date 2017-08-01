28a0f4604bdb70b15fce7799c487b80d16832810
Bunks18 <wbunker11@gmail.com>
2017-08-01 07:44:52 -0400

    started css

--- src/components/GoalShow.js
+++ src/components/GoalShow.js
@@ -3,36 +3,38 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchGoal } from '../actions/goal';
 import Reflection from './Reflection';
+import '../App.css';
 class GoalShow extends Component {
-
-
-
   componentDidMount() {
     const { id } = this.props.match.params;
-    this.props.fetchGoal(id)
+    this.props.fetchGoal(id);
   }
 
   render() {
     if (!this.props.goal) {
-      return <h1>Loading....</h1>
+      return <h1>Loading....</h1>;
     }
     const { title, reflections } = this.props.goal;
     console.log('reflections', reflections);
-   
+
     return (
-      <div>
+      <div className="dashboard">
         {title}
-      <Reflection goalId={this.props.match.params.id} />
-        <ul>
+        <Reflection goalId={this.props.match.params.id} />
+        <div className="">
           {reflections.map(ref => {
             return (
-              <li key={ref._id}>
-                {ref.reflectionContent}
-              </li>
+              <div>
+              <h3>Date Goes Here</h3>
+                <p key={ref._id}>
+                  {ref.reflectionContent}
+                  <br/>
+                </p>
+              </div>
             );
           })}
-        </ul>
-      <Link to="/">Back To Index</Link>
+        </div>
+        <Link to="/">Back To Index</Link>
       </div>
     );
   }
@@ -40,7 +42,7 @@ class GoalShow extends Component {
 
 function mapStateToProps(state, ownProps) {
   return {
-    goal: state.goal.goals.find(g => g._id === ownProps.match.params.id),
+    goal: state.goal.goals.find(g => g._id === ownProps.match.params.id)
   };
 }
 
